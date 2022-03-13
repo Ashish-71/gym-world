4deec1a5bd79031d303dc0b8aedb73bafd702843
devarsh007 <76660596+devarsh007@users.noreply.github.com>
2022-03-13 14:16:11 +0530

    Update gym_db.sql

--- GymEcomerce/Database/gym_db.sql
+++ GymEcomerce/Database/gym_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 08, 2022 at 09:55 AM
+-- Generation Time: Mar 13, 2022 at 09:45 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -52,6 +52,41 @@ CREATE TABLE `categories` (
   `status` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `categories`, `status`) VALUES
+(16, 'cardio', 1),
+(17, 'Equipment’s', 1),
+(18, 'powders', 1),
+(19, 'plates', 1),
+(20, 'benches', 1),
+(21, 'merchants', 1),
+(22, 'accessries', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_us`
+--
+
+CREATE TABLE `contact_us` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `comment` text NOT NULL,
+  `add_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact_us`
+--
+
+INSERT INTO `contact_us` (`id`, `name`, `email`, `mobile`, `comment`, `add_on`) VALUES
+(2, 'ashish', 'ashishsoni@gmail.com', '1234567890', 'kevi website banai chhe chodya', '2022-03-10 06:01:48');
+
 -- --------------------------------------------------------
 
 --
@@ -68,11 +103,43 @@ CREATE TABLE `product` (
   `image` varchar(255) NOT NULL,
   `short_desc` varchar(2000) NOT NULL,
   `description` text NOT NULL,
-  `meta_title` text NOT NULL,
+  `meta_title` varchar(2000) NOT NULL,
   `meta_keyword` varchar(2000) NOT NULL,
   `meta_desc` varchar(2000) NOT NULL,
   `status` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `categories_id`, `name`, `mrp`, `price`, `qty`, `image`, `short_desc`, `description`, `meta_title`, `meta_keyword`, `meta_desc`, `status`) VALUES
+(10, 22, 'product_1', 600, 500, 5, '693125415_1.jpg', 'asd', 'fgh', 'jkl', 'fgh', 'asd', 1),
+(11, 20, 'product_2', 900, 500, 6, '566100525_2.jpg', 'jklasd', 'asd', 'asd', 'jkl', 'fgh', 1),
+(12, 16, 'product_3', 700, 600, 5, '764420004_3.jpg', 'qw', 'qwe', 'qwe', 'qwe', 'qwe', 1),
+(13, 16, '2223455', 800, 300, 5, '130058301_4.jpg', 'ahztk', 'qwe', 'dfgj', 'ert', 'cvbn', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `add_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `email`, `mobile`, `add_on`) VALUES
+(1, 'ashish', '', 'ashishsoni@gmail.com', '1234567890', '2022-03-10 05:57:58');
 
 --
 -- Indexes for dumped tables
@@ -91,9 +158,21 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -110,13 +189,25 @@ ALTER TABLE `admin_user`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
