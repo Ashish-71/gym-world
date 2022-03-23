db5dd1f085e8ec118f28c19041cfcd1c59659107
devarsh007 <76660596+devarsh007@users.noreply.github.com>
2022-03-23 15:11:44 +0530

    Update add_to_cart.inc.php

--- GymEcomerce/add_to_cart.inc.php
+++ GymEcomerce/add_to_cart.inc.php
@@ -1,23 +1,33 @@
 <?php
 class add_to_cart{
-    function addproduct($pid,$qty){
-        $_SESSION['cart'][$pid]['qty']=$qty; 
-    }
+	function addProduct($pid,$qty){
+		$_SESSION['cart'][$pid]['qty']=$qty;
+	}
+	
+	function updateProduct($pid,$qty){
+		if(isset($_SESSION['cart'][$pid])){
+			$_SESSION['cart'][$pid]['qty']=$qty;
+		}
+	}
+	
+	function removeProduct($pid){
+		if(isset($_SESSION['cart'][$pid])){
+			unset($_SESSION['cart'][$pid]);
+		}
+	}
+	
+	function emptyProduct(){
+		unset($_SESSION['cart']);
+	}
+	
+	function totalProduct(){
+		if(isset($_SESSION['cart'])){
+			return count($_SESSION['cart']);
+		}else{
+			return 0;
+		}
+		
+	}
 
-    function updateproduct($pid,$qty){
-        if(isset($_SESSION['cart'][$pid])){
-            $_SESSION['cart'][$pid]['qty']=$qty;
-        }
-    }
-
-    function removeproduct($pid){
-        if(isset($_SESSION['cart'][$pid])){
-            unset($_SESSION['cart'][$pid]);
-        }
-    }
-
-    function emptyproduct(){
-        unset($_SESSION['cart']);
-    }
 }
 ?>
