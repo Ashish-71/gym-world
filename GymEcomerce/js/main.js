65fb3ad5d068f7442db8b601eda70da54f617a28
devarsh007 <76660596+devarsh007@users.noreply.github.com>
2022-03-23 15:12:04 +0530

    Update main.js

--- GymEcomerce/js/main.js
+++ GymEcomerce/js/main.js
@@ -460,3 +460,21 @@ function send_message(){
 	}
 }
 
+function manage_cart(pid,type){
+	if(type=='update'){
+		var qty=jQuery("#"+pid+"qty").val();
+	}else{
+		var qty=jQuery("#qty").val();
+	}
+	jQuery.ajax({
+		url:'manage_cart.php',
+		type:'post',
+		data:'pid='+pid+'&qty='+qty+'&type='+type,
+		success:function(result){
+			if(type=='update' || type=='remove'){
+				window.location.href='cart.php';
+			}
+			jQuery('.htc__qua').html(result);
+		}	
+	});	
+}
