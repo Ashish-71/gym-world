001423a8378fba7a3721fc2cda584949340c91ff
Ashish-71 <98458318+Ashish-71@users.noreply.github.com>
2022-04-06 15:36:47 +0530

    Update connection.inc.php

--- GymEcomerce/connection.inc.php
+++ GymEcomerce/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $con=mysqli_connect("localhost","root","","gym_db");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'gym-world/GymEcomerce/');
-define('SITE_PATH','http://127.0.0.1/gym-world/GymEcomerce/');
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'GymEcomerce/');
+define('SITE_PATH','http://localhost:3000/GymEcomerce/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
