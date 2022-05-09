5bf6b5a753f6872f930d838e32a1728600df519d
devarsh007 <76660596+devarsh007@users.noreply.github.com>
2022-05-09 18:02:46 +0530

    Update functions.inc.php

--- GymEcomerce/admin/functions.inc.php
+++ GymEcomerce/admin/functions.inc.php
@@ -57,11 +57,16 @@ function generateShipRocketToken($con){
   ));
   $SR_login_Response = curl_exec($curl);
   curl_close($curl);
-  $SR_login_Response_out = json_decode($SR_login_Response);
-  $token = $SR_login_Response_out->{'token'};
-  $added_on=date('Y-m-d h:i:s');
-  mysqli_query($con,"update shiprocket_token set token='$token',added_on='$added_on' where id=1");
-  return $token;
+  $SR_login_Response_out = json_decode($SR_login_Response,true);
+  
+  if(isset($SR_login_Response_out['token'])){
+	  $token = $SR_login_Response_out['token'];
+	  $added_on=date('Y-m-d h:i:s');
+	  mysqli_query($con,"update shiprocket_token set token='$token',added_on='$added_on' where id=1");
+	  return $token;
+  }else{
+	  echo "<div class='shiprocket_error'>Token Error: ".$SR_login_Response_out['message'].'</div>';
+  }
 }
 
 function placeShipRocketOrder($con,$token,$order_id){
@@ -76,6 +81,12 @@ function placeShipRocketOrder($con,$token,$order_id){
 	$address=$row_order['address'];
 	$pincode=$row_order['pincode'];
 	$city=$row_order['city'];
+	
+	$length=$row_order['length'];
+	$breadth=$row_order['breadth'];
+	$height=$row_order['height'];
+	$weight=$row_order['weight'];
+	
 	$payment_type=$row_order['payment_type'];
 	if($payment_type=='payu'){
 		$payment_type='Prepaid';
@@ -112,14 +123,14 @@ function placeShipRocketOrder($con,$token,$order_id){
 		CURLOPT_CUSTOMREQUEST => "POST",
 		CURLOPT_POSTFIELDS =>'{"order_id": "'.$order_id.'",
 	  "order_date": "'.$order_date.'",
-	  "pickup_location": "Ahmedabad",
+	  "pickup_location": "Delhi",
 	  "billing_customer_name": "'.$name.'",
 	  "billing_last_name": "",
 	  "billing_address": "'.$address.'",
 	  "billing_address_2": "",
 	  "billing_city": "'.$city.'",
 	  "billing_pincode": "'.$pincode.'",
-	  "billing_state": "Delhi",
+	  "billing_state": "Ahmedabad",
 	  "billing_country": "India",
 	  "billing_email": "'.$email.'",
 	  "billing_phone": "'.$mobile.'",
@@ -143,10 +154,10 @@ function placeShipRocketOrder($con,$token,$order_id){
 	  "transaction_charges": 0,
 	  "total_discount": 0,
 	  "sub_total": "'.$total_price.'",
-	  "length": 10,
-	  "breadth": 15,
-	  "height": 20,
-	  "weight": 2.5
+	  "length": '.$length.',
+	  "breadth": '.$breadth.',
+	  "height": '.$height.',
+	  "weight": '.$weight.'
 		}',
     CURLOPT_HTTPHEADER => array(
       "Content-Type: application/json",
@@ -155,8 +166,65 @@ function placeShipRocketOrder($con,$token,$order_id){
   ));
   $SR_login_Response = curl_exec($curl);
   curl_close($curl);
-  //$SR_login_Response_out = json_decode($SR_login_Response);
-  echo '<pre>';
-  print_r($SR_login_Response);
+  $SR_login_Response_out = json_decode($SR_login_Response,true);
+  
+  if(isset($SR_login_Response_out['order_id']) && isset($SR_login_Response_out['shipment_id'])){
+	  $ship_order_id=$SR_login_Response_out['order_id'];
+	  $ship_shipment_id=$SR_login_Response_out['shipment_id'];
+	  mysqli_query($con,"update `order` set ship_order_id='$ship_order_id', ship_shipment_id='$ship_shipment_id' where id='$order_id'");
+	  echo "Order id:-".$ship_order_id.'<br/>';
+	  echo "Shipment id:-".$ship_shipment_id.'<br/>';
+  }else{
+	  $errroHTML='';
+	  if(isset($SR_login_Response_out['errors'])){
+		  foreach($SR_login_Response_out['errors'] as $key=>$val){
+			  $errroHTML.=$key.' : '.$val[0].'<br/>';
+		  }
+	  }else{
+		  $errroHTML="Shiprocket API: Someting went wrong";
+	  }
+	  echo "<div class='shiprocket_error'>".$errroHTML."</div>";
+	  
+  }
+}
+
+function cancelShipRocketOrder($token,$ship_order_id){
+	$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://apiv2.shiprocket.in/v1/external/orders/cancel",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS =>"{\n  \"ids\": [".$ship_order_id."]\n}",
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json",
+    "Authorization: Bearer $token"
+  ),
+));
+	$response = curl_exec($curl);
+	curl_close($curl);
+}
+
+
+function isAdmin(){
+	if(!isset($_SESSION['ADMIN_LOGIN'])){
+	?>
+		<script>
+		window.location.href='login.php';
+		</script>
+		<?php
+	}
+	if($_SESSION['ADMIN_ROLE']==1){
+		?>
+		<script>
+		window.location.href='product.php';
+		</script>
+		<?php
+	}
 }
 ?>
