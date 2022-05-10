77dba12b04542e67f971f04efcf4913f532db0ab
devarsh007 <76660596+devarsh007@users.noreply.github.com>
2022-05-10 15:41:26 +0530

    Update categories.php

--- GymEcomerce/categories.php
+++ GymEcomerce/categories.php
@@ -38,7 +38,7 @@ if(isset($_GET['sort'])){
 
 }
 
-if($cat_id>0 && ($sub_categories!='' && $sub_categories>0)){
+if($cat_id>0 ){
 	$get_product=get_product($con,'',$cat_id,'','',$sort_order,'',$sub_categories);
 }else{
 	?>
