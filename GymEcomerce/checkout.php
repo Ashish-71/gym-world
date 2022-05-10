b2d3c589fc36c126f6b54da2410b56b29f2a86fb
devarsh007 <76660596+devarsh007@users.noreply.github.com>
2022-05-10 17:21:34 +0530

    Update checkout.php

--- GymEcomerce/checkout.php
+++ GymEcomerce/checkout.php
@@ -61,65 +61,44 @@ if(isset($_POST['submit'])){
 	
 	unset($_SESSION['cart']);
 	
-	if($payment_type=='payu'){
-		$MERCHANT_KEY = "gtKFFx"; 
-		$SALT = "eCwWELxi";
-		$hash_string = '';
-		//$PAYU_BASE_URL = "https://secure.payu.in";
-		$PAYU_BASE_URL = "https://test.payu.in";
-		$action = '';
-		$posted = array();
-		if(!empty($_POST)) {
-		  foreach($_POST as $key => $value) {    
-			$posted[$key] = $value; 
-		  }
-		}
+	if($payment_type=='instamojo'){
 		
 		$userArr=mysqli_fetch_assoc(mysqli_query($con,"select * from users where id='$user_id'"));
 		
-		$formError = 0;
-		$posted['txnid']=$txnid;
-		$posted['amount']=$total_price;
-		$posted['firstname']=$userArr['name'];
-		$posted['email']=$userArr['email'];
-		$posted['phone']=$userArr['mobile'];
-		$posted['productinfo']="productinfo";
-		$posted['key']=$MERCHANT_KEY ;
-		$hash = '';
-		$hashSequence = "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10";
-		if(empty($posted['hash']) && sizeof($posted) > 0) {
-		  if(
-				  empty($posted['key'])
-				  || empty($posted['txnid'])
-				  || empty($posted['amount'])
-				  || empty($posted['firstname'])
-				  || empty($posted['email'])
-				  || empty($posted['phone'])
-				  || empty($posted['productinfo'])
-				 
-		  ) {
-			$formError = 1;
-		  } else {    
-			$hashVarsSeq = explode('|', $hashSequence);
-			foreach($hashVarsSeq as $hash_var) {
-			  $hash_string .= isset($posted[$hash_var]) ? $posted[$hash_var] : '';
-			  $hash_string .= '|';
-			}
-			$hash_string .= $SALT;
-			$hash = strtolower(hash('sha512', $hash_string));
-			$action = $PAYU_BASE_URL . '/_payment';
-		  }
-		} elseif(!empty($posted['hash'])) {
-		  $hash = $posted['hash'];
-		  $action = $PAYU_BASE_URL . '/_payment';
-		}
-
-
-		$formHtml ='<form method="post" name="payuForm" id="payuForm" action="'.$action.'"><input type="hidden" name="key" value="'.$MERCHANT_KEY.'" /><input type="hidden" name="hash" value="'.$hash.'"/><input type="hidden" name="txnid" value="'.$posted['txnid'].'" /><input name="amount" type="hidden" value="'.$posted['amount'].'" /><input type="hidden" name="firstname" id="firstname" value="'.$posted['firstname'].'" /><input type="hidden" name="email" id="email" value="'.$posted['email'].'" /><input type="hidden" name="phone" value="'.$posted['phone'].'" /><textarea name="productinfo" style="display:none;">'.$posted['productinfo'].'</textarea><input type="hidden" name="surl" value="'.SITE_PATH.'payment_complete.php" /><input type="hidden" name="furl" value="'.SITE_PATH.'payment_fail.php"/><input type="submit" style="display:none;"/></form>';
-		echo $formHtml;
-		echo '<script>document.getElementById("payuForm").submit();</script>';
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://test.instamojo.com/api/1.1/payment-requests/');
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER,
+			array("X-Api-Key:test_e2b6e8f593ab5ab5e614682c2b1","X-Auth-Token:test_a0f38237c6fa9d3b809efbe60b7")
+		);
+		
+		$payload = Array(
+			'purpose' => 'Buy Product',
+			'amount' => $total_price,
+			'phone' => $userArr['mobile'],
+			'buyer_name' => $userArr['name'],
+			'redirect_url' => 'http://localhost:3000/GymEcomerce/payment_complete.php',
+			'send_email' => false,
+			'send_sms' => false,
+			'email' => $userArr['email'],
+			'allow_repeated_payments' => false
+		);
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+		$response = curl_exec($ch);
+		curl_close($ch); 
+		$response=json_decode($response);
+		$_SESSION['TID']=$response->payment_request->id;
+		mysqli_query($con,"update `order` set txnid='".$response->payment_request->id."' where id='".$order_id."'");
+		?>
+		<script>
+		window.location.href='<?php echo $response->payment_request->longurl?>';
+		</script>
+		<?php
 	}else{	
-		sentInvoice($con,$order_id);
+		//sentInvoice($con,$order_id);
 		?>
 		<script>
 			window.location.href='thank_you.php';
@@ -258,7 +237,7 @@ if(isset($_POST['submit'])){
 											<div class="paymentinfo">
 												<div class="single-method">
 													COD <input type="radio" name="payment_type" value="COD" required/>
-													&nbsp;&nbsp;PayU <input type="radio" name="payment_type" value="payu" required/>
+													&nbsp;&nbsp;Instamojo <input type="radio" name="payment_type" value="instamojo" required/>
 												</div>
 												<div class="single-method">
 												  
