d12f25e469dd40d2c87ca2839f0e259a1ac633b1
devarsh007 <76660596+devarsh007@users.noreply.github.com>
2022-05-10 10:04:18 +0530

    Update checkout.php

--- GymEcomerce/checkout.php
+++ GymEcomerce/checkout.php
@@ -1,16 +1,9 @@
 <?php 
 require('top.php');
-if(count($_SESSION['cart'])==0){
+if(!isset($_SESSION['cart']) || count($_SESSION['cart'])==0){
 	?>
 	<script>
 		window.location.href='index.php';
-	</script>
-	<?php
-}
-if(!isset($_SESSION['USER_LOGIN'])){
-	?>
-	<script>
-		window.location.href='login.php';
 	</script>
 	<?php
 }
@@ -39,9 +32,22 @@ if(isset($_POST['submit'])){
 	$added_on=date('Y-m-d h:i:s');
 	
 	$txnid = substr(hash('sha256', mt_rand() . microtime()), 0, 20);
-		
 	
-	mysqli_query($con,"insert into `order`(user_id,address,city,pincode,payment_type,payment_status,order_status,added_on,total_price,txnid) values('$user_id','$address','$city','$pincode','$payment_type','$payment_status','$order_status','$added_on','$total_price','$txnid')");
+	if(isset($_SESSION['COUPON_ID'])){
+		$coupon_id=$_SESSION['COUPON_ID'];
+		$coupon_code=$_SESSION['COUPON_CODE'];
+		$coupon_value=$_SESSION['COUPON_VALUE'];
+		$total_price=$total_price-$coupon_value;
+		unset($_SESSION['COUPON_ID']);
+		unset($_SESSION['COUPON_CODE']);
+		unset($_SESSION['COUPON_VALUE']);
+	}else{
+		$coupon_id='';
+		$coupon_code='';
+		$coupon_value='';	
+	}	
+	
+	mysqli_query($con,"insert into `order`(user_id,address,city,pincode,payment_type,payment_status,order_status,added_on,total_price,txnid,coupon_id,coupon_code,coupon_value) values('$user_id','$address','$city','$pincode','$payment_type','$payment_status','$order_status','$added_on','$total_price','$txnid','$coupon_id','$coupon_code','$coupon_value')");
 	
 	$order_id=mysqli_insert_id($con);
 	
@@ -188,7 +194,29 @@ if(isset($_POST['submit'])){
                                                 </div>
                                                 <div class="col-md-6">
                                                     <div class="checkout-method__login">
-                                                      
+                                                        <form action="#">
+                                                            <h5 class="checkout-method__title">Register</h5>
+                                                            <div class="single-input">
+                                                                <input type="text" name="name" id="name" placeholder="Your Name*" style="width:100%">
+																<span class="field_error" id="name_error"></span>
+                                                            </div>
+															<div class="single-input">
+                                                                <input type="text" name="email" id="email" placeholder="Your Email*" style="width:100%">
+																<span class="field_error" id="email_error"></span>
+                                                            </div>
+															
+                                                            <div class="single-input">
+                                                                <input type="text" name="mobile" id="mobile" placeholder="Your Mobile*" style="width:100%">
+																<span class="field_error" id="mobile_error"></span>
+                                                            </div>
+															<div class="single-input">
+                                                                <input type="password" name="password" id="password" placeholder="Your Password*" style="width:100%">
+																<span class="field_error" id="password_error"></span>
+                                                            </div>
+                                                            <div class="dark-btn">
+                                                                <button type="button" class="fv-btn" onclick="user_register()">Register</button>
+                                                            </div>
+                                                        </form>
                                                     </div>
                                                 </div>
                                             </div>
@@ -237,9 +265,7 @@ if(isset($_POST['submit'])){
 												</div>
 											</div>
 										</div>
-										<div class="submit_btn">
-										 <input type="submit" name="submit"/>
-									</div>
+										 <input type="submit" name="submit" class="fv-btn"/>
 									</form>
                                 </div>
                             </div>
@@ -274,14 +300,56 @@ if(isset($_POST['submit'])){
                                 </div>
 								<?php } ?>
                             </div>
+							<div class="ordre-details__total" id="coupon_box">
+                                <h5>Coupon Value</h5>
+                                <span class="price" id="coupon_price"></span>
+                            </div>
                             <div class="ordre-details__total">
                                 <h5>Order total</h5>
-                                <span class="price"><?php echo $cart_total?></span>
+                                <span class="price" id="order_total_price"><?php echo $cart_total?></span>
                             </div>
+							
+							<div class="ordre-details__total bilinfo">
+                                <input type="textbox" id="coupon_str" class="coupon_style mr5"/> <input type="button" name="submit" class="fv-btn coupon_style" value="Apply Coupon" onclick="set_coupon()"/>
+								
+                            </div>
+							<div id="coupon_result"></div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        						
-<?php require('footer.php')?>        
+        <script>
+			function set_coupon(){
+				var coupon_str=jQuery('#coupon_str').val();
+				if(coupon_str!=''){
+					jQuery('#coupon_result').html('');
+					jQuery.ajax({
+						url:'set_coupon.php',
+						type:'post',
+						data:'coupon_str='+coupon_str,
+						success:function(result){
+							var data=jQuery.parseJSON(result);
+							if(data.is_error=='yes'){
+								jQuery('#coupon_box').hide();
+								jQuery('#coupon_result').html(data.dd);
+								jQuery('#order_total_price').html(data.result);
+							}
+							if(data.is_error=='no'){
+								jQuery('#coupon_box').show();
+								jQuery('#coupon_price').html(data.dd);
+								jQuery('#order_total_price').html(data.result);
+							}
+						}
+					});
+				}
+			}
+		</script>		
+<?php 
+if(isset($_SESSION['COUPON_ID'])){
+	unset($_SESSION['COUPON_ID']);
+	unset($_SESSION['COUPON_CODE']);
+	unset($_SESSION['COUPON_VALUE']);
+}
+require('footer.php');
+?>        
